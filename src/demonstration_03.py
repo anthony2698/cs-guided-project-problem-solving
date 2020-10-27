993bba5779a22e3827eaaf3a3781954c6b48b9b9
Anthony Carrillo <carrilloanthony2698@gmail.com>
2020-10-27 17:49:05 -0400

    Completed demo 3.

--- src/demonstration_03.py
+++ src/demonstration_03.py
@@ -13,5 +13,22 @@ Notes:
 - Bonus: Try to complete this challenge in one line!
 """
 def multiply_nums(nums):
-    # Your code here
+    # Split the nums string
+    # Iterate through list and multiply the nums
+    # return product    
+
+    int_digits = nums.split(', ')
+    product = 1
+
+    for num in int_digits:
+        int_num = int(num)
+        product *= int_num
+
+    return product
+
+
+print(multiply_nums("2, 3"))
+print(multiply_nums("1, 2, 3, 4"))
+print(multiply_nums("54, 75, 453, 0"))
+print(multiply_nums("10, -2"))
 
