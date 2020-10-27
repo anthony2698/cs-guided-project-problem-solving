ba5859c1e591c093c7aa2a52856507841d12b991
Anthony Carrillo <carrilloanthony2698@gmail.com>
2020-10-27 16:35:22 -0400

    Completed task 6.

--- src/demonstration_06.py
+++ src/demonstration_06.py
@@ -9,4 +9,21 @@ The input string will only consist of lower case letters and/or spaces.
 """
 def get_count(input_str):
     # Your code here
+    # count number of vowels
+    # keep a counter to keep track of vowels
+    # we need to inspect every char in string
+    # keep a list or string of all the vowels we care about
+    # iterate through the characters of our input string
+        # as we iterate we can check if the current char is a part of the vowel string/list
+        # if it is increment counter
+    # return counter
+    count = 0
+    vowels = 'aeiou'
+    for char in input_str:
+        if char in vowels:
+            count += 1
+        
+    return count
 
+print(get_count("aeioufff"))
+        
